d203731544ca78fb317e45bfe8fb695756305d7a
Vladislav-creator <valdisbeketov@gmail.com>
2023-08-14 09:25:20 +0300

    88

--- js/task-02.js
+++ js/task-02.js
@@ -1,52 +1,8 @@
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
-const listEl = document.querySelector("#ingredients");
-const elementsToAdd = [];
-ingredients.forEach((el) => {
-  const liEl = document.createElement("li");
-  liEl.className = "item";
-  liEl.textContent = el;
-  elementsToAdd.push(liEl);
-});
-listEl.append(...elementsToAdd);
-console.log(elementsToAdd);
-
-//2option
-// const listEl = document.querySelector("#ingredients");
-// const listElstr = ingredients
-//   .map((liElvalue) => `<li class="item">${liElvalue}</li>`)
-//   .join("");
-// listEl.innerHTML = listElstr;
-
-//3option
-// const listEl = document.querySelector("#ingredients");
-// ingredients.forEach((el) => {
-//   const liEl = document.createElement("li");
-//   liEl.className = "item";
-//   liEl.textContent = el;
-//   listEl.append(liEl);
-// });
-// console.log(listEl);
-const form = document.querySelector(".form");
-
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const {
-    elements: { login, password },
-  } = event.currentTarget;
-
-  if (login.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
-
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
--- js/task-03.js
+++ js/task-03.js
@@ -1,52 +1,14 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
-const list = document.querySelector(".gallery");
-console.log(list);
-const markup = images
-  .map(
-    (image) =>
-      `<li class="list-item"><img src="${image.url}" alt="${image.alt}" width = '300px' height = '200px'></li>`
-  )
-  .join("");
-list.insertAdjacentHTML("beforeend", markup);
-const menuItemsByTagName = document.querySelector("ul");
-menuItemsByTagName.style.listStyle = "none";
-menuItemsByTagName.style.display = "flex";
-menuItemsByTagName.style.gap = "50px";
-menuItemsByTagName.style.justifyContent = "space-evenly";
-
-const btn = document.querySelector(".btn");
-const square = document.querySelector(".square");
-const bodyWidth = document.body.clientWidth - 50;
-let currentPosition = 0;
-let movingRight = true;
-btn.addEventListener("click", () => {
-  if (movingRight) {
-    currentPosition += 50;
-    btn.textContent = "Right";
-    if (currentPosition >= bodyWidth) {
-      currentPosition = bodyWidth;
-      movingRight = false;
-    }
-  } else {
-    btn.textContent = "Left";
-    currentPosition -= 50;
-    if (currentPosition <= 0) {
-      currentPosition = 0;
-      movingRight = true;
-    }
-  }
-  square.style.left = currentPosition + "px";
-});
